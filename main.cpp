696e4860902d36a4a5e48b70a6c533140c574747
Legitmod17 <neshkin2018@yandex.ru>
2026-01-23 14:56:45 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(0, "");
+	cout << "\x1b[6m[+]Введите положительное число для старта обратного отчёта!\x1b[0m\n";
+	int num;
+	cin >> num;
+}
